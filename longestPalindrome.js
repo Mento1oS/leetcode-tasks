967e64ce6385d372e43057399ae5f285b10c6f43
Artem_Mentolos <mentolosscg@yandex.ru>
2024-06-04 21:44:09 +0300

    longest palindrome possible

---
+++ longestPalindrome.js
@@ -0,0 +1,29 @@
+/** @format */
+
+const longestPalindrome = function (s) {
+  const map = new Map();
+  let sum = 0;
+  let odds = false;
+  for (const element of s) {
+    if (map.has(element)) {
+      map.set(element, map.get(element) + 1);
+    } else {
+      map.set(element, 1);
+    }
+  }
+  map.forEach((elem) => {
+    if (elem % 2 === 0) {
+      sum += elem;
+    } else {
+      odds = true;
+      sum += elem - 1;
+    }
+  });
+  return sum + odds ?? 1;
+};
+
+console.log(
+  longestPalindrome(
+    "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
+  )
+);
